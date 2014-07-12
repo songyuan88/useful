e676bb4e757b75b84e5f6aa0ece9c5532c448a63
Alex Fang <frjalex@gmail.com>
2014-07-12 13:51:50 -0400

    Create ipsecauto_centos.sh

---
+++ ipsecauto_centos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+#IPsecAuto version 0.1-beta-centos  by Alex Fang
+#
+
+echo "+------------------------------------------------------+"
+echo "|IPsecAuto version 0.1-beta by Alex Fang               |"
+echo "|Automated installation script for Centos 
+echo "|
+echo "|
+echo "|
+echo "|
+echo "|
+echo "|
