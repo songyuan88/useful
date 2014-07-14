b523fcda8e92bae4cfbf142c5a565cf342e0afd7
Alex Fang <frjalex@gmail.com>
2014-07-15 03:15:02 +0800

    Update racoon_config_setup.sh

--- racoon_config_setup.sh
+++ racoon_config_setup.sh
@@ -16,6 +16,25 @@ echo "+----------------------------------------------------------------+"
 echo
 
 echo "Press enter to continue..." $entercontinue ; read entercontinue
+
+echo "Press 1 if u ONLY WANT to edit the iptables rules." $entercontinue2 ; read entercontinue2
+
+
+if [ $entercontinue2 = 1 ] then
+  echo "Custom and edit your racoon.conf by yourself after install. Enter to set iptables rules:" read entercontinue ;
+  $CLIENTIP=10.1.0.0/24
+  echo "enter your Client IP Address of racoon.conf. Enter to use the default(recommend)" $CLIENTIP ; read CLIENTIP
+  apt-get update && apt-get -y install iptables
+  iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+  iptables -A INPUT -p udp -m udp --dport 500 -j ACCEPT
+  iptables -A INPUT -p udp -m udp --dport 4500 -j ACCEPT
+  iptables -A INPUT -p esp -j ACCEPT
+  iptables -A INPUT -s 127.0.0.0/24 -d 127.0.0.0/24 -j ACCEPT
+  iptables -A FORWARD -s $CLIENTIP -j ACCEPT
+  iptables -t nat -A POSTROUTING -s $CLIENTIP -o eth0 -j MASQUERADE
+fi
+
+
 echo "-----------------Downloading Config File---------------------"
 FILEPATH=/etc/racoon
 echo "Please enter your path your racoon was installed. The default path will be /etc/racoon." read FILEPATH ;
@@ -34,7 +53,9 @@ iptables -A INPUT -p udp -m udp --dport 500 -j ACCEPT
 iptables -A INPUT -p udp -m udp --dport 4500 -j ACCEPT
 iptables -A INPUT -p esp -j ACCEPT
 iptables -A INPUT -s 127.0.0.0/24 -d 127.0.0.0/24 -j ACCEPT
-iptables -A FORWARD -s 10.1.0.0/24 -j ACCEPT
+iptables -A FORWARD -s $CLIENTIP -j ACCEPT
+iptables -t nat -A POSTROUTING -s $CLIENTIP -o eth0 -j MASQUERADE
+
 
 echo "Finished.   Bye. Exiting..."
 #FINISHED.
