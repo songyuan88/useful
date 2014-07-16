208d86c3d5458248f2f9f468c5c278a3344b5e05
Alex Fang <frjalex@gmail.com>
2014-07-17 03:39:43 +0800

    Create pigsay.sh

---
+++ pigsay.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+echo "what does the pig gonna say?" $YOURSAY ; read YOURSAY
+echo "+---------------+
+echo "|" $YOURSAY
+echo "+---------------+                     /"
+echo "      \   ^__^                       /"
+echo "       \  (oo)\/-------------------\/"
+echo "        ( /00\)\--------------------\"
+echo "         \  ^ /|   |-------------w   |"
+echo "               |   |             |   |"
+echo "-------------------------------------------------------"
+echo "Copyright (c) Alex Fang. Inspired by 'cowsay'"
+
+exit 1
+#end#
